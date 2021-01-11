fa7c1ab201bb356b80e30085d02694039704a219
Joe Panetta <jlapnetta1681@icloud.com>
2021-01-10 23:43:55 -0500

    Added the right side widget, and styling it

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./Header"
 import './App.css';
 import SideBar from "./Sidebar"
 import Feed from "./Feed"
+import Widgets from "./Widgets"
 
 function App() {
   return (
@@ -11,11 +12,14 @@ function App() {
 
       <Header />
       <div className="app_body">
+
+        <div id="fb-root"></div>
+
         <SideBar />
         <Feed />
+        <Widgets />
 
-        {/* Feed */}
-        {/* Widgets */}
+
       </div>
 
     </div>
